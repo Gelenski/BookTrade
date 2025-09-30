2d64473a641c8e2dd3685fd9b18d3bcd979d2f18
Lucas Gelenski <gelenskilucas@gmail.com>
2025-09-30 20:07:52 -0300

    Corrigindo variaveis

--- db/database.js
+++ db/database.js
@@ -1,11 +1,12 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
 const pool = mysql.createPool({
-  host: "localhost", // MySQL local
-  user: "root",
-  password: "GelenskiRoot.07",
-  database: "booktrade",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
-module.exports = pool.promise(); // Usamos Promises para async/await
+module.exports = pool.promise();
