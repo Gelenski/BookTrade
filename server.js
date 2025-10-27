6eecffd9ec6a4958d7f5d50c3f8c979c3f7fb533
Lucas Gelenski <gelenskilucas@gmail.com>
2025-10-27 08:44:26 -0300

    Adição da rota que retorna os livros pendentes para revisão Adição da função de verificação do revisor

--- server.js
+++ server.js
@@ -42,6 +42,20 @@ function verificarAutenticacao(req, res, next) {
   });
 }
 
+function verificarRevisor(req, res, next) {
+  if (
+    req.session &&
+    req.session.usuario &&
+    req.session.usuario.tipo === "revisor"
+  ) {
+    return next();
+  }
+  return res.status(403).json({
+    success: false,
+    message: "Acesso negado. Apenas revisores.",
+  });
+}
+
 function verificarAdmin(req, res, next) {
   if (
     req.session &&
@@ -614,6 +628,33 @@ app.post("/api/cadastrar-livro", verificarAutenticacao, async (req, res) => {
       .json({ success: false, message: "Erro no cadastro: " + err.message });
   }
 });
+
+app.get(
+  "/api/livros-pendentes",
+  verificarAutenticacao,
+  verificarRevisor,
+  async (req, res) => {
+    try {
+      const [results] = await db.query(
+        `SELECT l.id_livro, l.titulo, l.descricao, l.ano_publicacao, l.isbn, l.estado, l.data_postagem,
+                a.nome AS nome_autor, a.nacionalidade AS nacionalidade_autor, g.nome AS nome_genero,
+                u.id_usuario, u.nome AS nome_usuario, u.email AS email_usuario
+         FROM livro l 
+          INNER JOIN autor a ON l.id_autor = a.id_autor
+          INNER JOIN genero g ON l.id_genero = g.id_genero
+          INNER JOIN usuario u ON l.id_usuario = u.id_usuario
+         WHERE l.aprovado IS NULL`
+      );
+      res.json({ success: true, livros: results });
+    } catch (err) {
+      console.error("Erro ao listar livros pendentes:", err);
+      res.status(500).json({
+        success: false,
+        message: "Erro no servidor",
+      });
+    }
+  }
+);
 
 // TODO: Rota de geração de relatório (ADMIN)
 // app.get("/api/relatorio", async (req, res) => {});
